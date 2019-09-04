7089a06da55a888efdfb7d22a86e8b4b84076b27
officer <kasaiso@amazon.com>
2019-09-04 18:35:42 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    var status; 
+    if(undefined == req.query.status){
+        status = 200;
+    }else{
+        status = Number(req.query.status);
+    }
+
+    res.sendStatus(status);
+
+});
+
+app.listen(3000);
